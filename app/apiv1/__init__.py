bf89dad45a5f31e137341bc367e20fa44d344f3e
wangbicong <wangbicong@hotmail.com>
2016-12-24 17:31:25 +0800

    add robot api(get)

--- app/apiv1/__init__.py
+++ app/apiv1/__init__.py
@@ -5,4 +5,4 @@ from flask_restful import Api
 apiv1_bp = Blueprint('apiv1', __name__)
 api = Api(apiv1_bp)
 
-from . import views
+from . import views, robot
--- app/apiv1/robot.py
+++ app/apiv1/robot.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+import json
+import requests
+from flask import jsonify
+from flask_restful import Resource
+from . import api
+
+KEY = '393d7387723348a79ea7cd846535eb3b'
+URL = 'http://www.tuling123.com/openapi/api'
+
+
+class Robot(Resource):
+
+    def get(self, info):
+
+        data = {
+            'key': KEY,
+            'info': info
+        }
+
+        result = requests.post(URL, data=data)
+        return jsonify(json.loads(result.content))
+
+api.add_resource(Robot, '/robot/<info>/')
