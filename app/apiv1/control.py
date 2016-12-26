ec73846b72bc84096653f729e204eb8fd7090a1e
wangbicong <wangbicong@hotmail.com>
2016-12-26 14:56:07 +0800

    add auth and control init

--- app/apiv1/control.py
+++ app/apiv1/control.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+from flask import jsonify
+from flask_restful import Resource
+from . import api
+
+
+class Control(Resource):
+
+    def post(self):
+
+
+        return jsonify({'code': '200'})
+
+api.add_resource(Control, '/control/')
