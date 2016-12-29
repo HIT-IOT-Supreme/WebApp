283f98c63711920f9abcc387e77f35c84d78b54a
wangbicong <wangbicong@hotmail.com>
2016-12-29 10:29:49 +0800

    add thread

--- app/utils/controller.py
+++ app/utils/controller.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
+import thread
 
 
 def break_up():
-    os.system('sudo python scripts/sunRaise.py')
+    thread.start_new_thread(shell_break_up, ())
 
 
 def play_music():
-    os.system('mpg123 static/demo.mp3')
+    pass
+    # thread.start_new_thread(shell_play_music, ())
 
-play_music()
+
+def shell_break_up():
+    os.system('sudo python scripts/sunRaise.py')
+
+
+def shell_play_music():
+    os.system('mpg123 static/demo.mp3')
